4f7dda24156f4ffae608e5284512652578f26faf
wulingxiao <wulingxiao@swifthealth.cn>
2021-01-26 11:38:20 +0800

    first commint

---
+++ src/main/java/com/example/demo/demo.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+/**
+ * @author rb
+ * @date 2021/1/26 上午11:16
+ * @description：
+ * @version: 1.0
+ */
+public class demo {
+    public static void main(string[] args) {
+
+    }
+
+
+}
+
+class mythread1 implements runnable{
+
+    @override
+    public void run() {
+
+    }
+}
